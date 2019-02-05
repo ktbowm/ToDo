5cbaa75717d2f20d50675fc246ea02a9d34bd6fc
ktbowm01 <kennedytb51@gmail.com>
2019-02-05 15:34:30 -0500

    Make item class

---
+++ WindowsFormsApp1/Item.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Item
+    {
+        //class variables
+        //+created DateTime? +importance ranking (use enum)?
+        private int itemId;
+        private string itemText;
+        private string itemDetails;
+        private bool itemIsComplete;
+        private DateTime itemDueDate;
+
+        //getters and setters (encapsulates variables)
+        public int ItemId { get => itemId; set => itemId = value; }
+        public string ItemText { get => itemText; set => itemText = value; }
+        public string ItemDetails { get => itemDetails; set => itemDetails = value; }
+        public bool ItemIsComplete { get => itemIsComplete; set => itemIsComplete = value; }
+        public DateTime ItemDueDate { get => itemDueDate; set => itemDueDate = value; }
+
+        //paramaterized constructor
+        public Item(int itemId, string itemText, string itemDetails, bool itemIsComplete, DateTime itemDueDate)
+        {
+            ItemId = itemId;
+            ItemText = itemText;
+            ItemDetails = itemDetails;
+            ItemIsComplete = itemIsComplete;
+            ItemDueDate = itemDueDate;
+        }
+
+        //default constructor
+        public Item()
+        {
+            ItemId = 0; //change to make sure id will be unique
+            ItemText = "Default Item Text";
+            ItemDetails = "Default Item Details";
+            ItemIsComplete = false;
+            ItemDueDate = DateTime.Now.AddDays(1); //change to another time? (default is tomorrow)
+        }
+
+        //copy constructor
+        public Item(Item item)
+        {
+            ItemId = item.itemId; //don't want same id even if it's a copy
+            ItemText = item.itemText;
+            ItemDetails = item.itemDetails;
+            ItemIsComplete = item.itemIsComplete;
+            ItemDueDate = item.itemDueDate;
+        }
+
+    }
+}
---
+++ WindowsFormsApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            //testing begin
+            Item i1 = new Item();
+            Item i2 = new Item(2, "i2 text", "i2 details", true, DateTime.Now);
+            Item i3 = new Item(i1);
+            i3.ItemText = "i3 text";
+            i3.ItemDetails = "i3 details";
+
+            Console.WriteLine("i1 id: {0}", i1.ItemId);
+            Console.WriteLine("i1 text: {0}", i1.ItemText);
+            Console.WriteLine("i1 details: {0}", i1.ItemDetails);
+            Console.WriteLine("i1 is complete: {0}", i1.ItemIsComplete);
+            Console.WriteLine("i1 due date: {0}", i1.ItemDueDate);
+
+            Console.WriteLine("i2 id: {0}", i2.ItemId);
+            Console.WriteLine("i2 text: {0}", i2.ItemText);
+            Console.WriteLine("i2 details: {0}", i2.ItemDetails);
+            Console.WriteLine("i2 is complete: {0}", i2.ItemIsComplete);
+            Console.WriteLine("i2 due date: {0}", i2.ItemDueDate);
+
+            Console.WriteLine("i3 id: {0}", i3.ItemId);
+            Console.WriteLine("i3 text: {0}", i3.ItemText);
+            Console.WriteLine("i3 details: {0}", i3.ItemDetails);
+            Console.WriteLine("i3 is complete: {0}", i3.ItemIsComplete);
+            Console.WriteLine("i3 due date: {0}", i3.ItemDueDate);
+            //testing end
+
+        }
+    }
+}
